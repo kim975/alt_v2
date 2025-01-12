ca97c7812292e29682790d9b70feeb9cf5ec43f1
kim975 <81149759+kim975@users.noreply.github.com>
2025-01-13 02:11:20 +0900

    test : 회원 가입 통합 테스트 작성
    - controller /client_join에 해당하는 행복경로 테스트 완료
    - service에서 회원가입을 위한 행복경로 테스트 완료

---
+++ src/test/java/com/alt/controller/LoginControllerTest.java
@@ -0,0 +1,112 @@
+package com.alt.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.alt.domain.ClientVO;
+import com.alt.domain.MemberVO;
+import com.alt.mapper.ClientMapper;
+import com.alt.service.ClientServiceImpl;
+import javax.servlet.ServletException;
+import lombok.Setter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletConfig;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {
+    "file:src/main/webapp/WEB-INF/spring/root-context.xml",
+    "file:src/main/webapp/WEB-INF/spring/security-context.xml",
+    "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
+})
+@WebAppConfiguration
+@Transactional
+public class LoginControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Setter(onMethod_ = @Autowired)
+    private ClientMapper clientMapper;
+
+    @Setter(onMethod_ = @Autowired)
+    private ClientServiceImpl clientService;
+
+    private DispatcherServlet dispatcherServlet;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    private ClientVO newClient;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.request = new MockHttpServletRequest();
+        this.response = new MockHttpServletResponse();
+        this.dispatcherServlet = new DispatcherServlet(webApplicationContext);
+        this.dispatcherServlet.init(new MockServletConfig());
+    }
+
+    @BeforeEach
+    public void createNewUser() throws Exception {
+        newClient = new ClientVO();
+        this.newClient.setCid("testId");
+        this.newClient.setCpassword("testPassword");
+        this.newClient.setCname("testName");
+        this.newClient.setCnick("testNickName");
+        this.newClient.setCphone("010-1234-1234");
+        this.newClient.setCaddress("서울시 강남구");
+    }
+
+    @Test
+    public void whenJoinNewClientThenSuccess() throws Exception {
+
+        //given
+        request.setMethod("POST");
+        request.setRequestURI("/client_join");
+
+        request.setParameter("cid", newClient.getCid());
+        request.setParameter("cnick", newClient.getCnick());
+        request.setParameter("cpassword", newClient.getCpassword());
+        request.setParameter("cname", newClient.getCname());
+        request.setParameter("cphone", newClient.getCphone());
+        request.setParameter("caddress", newClient.getCaddress());
+        request.setParameter("addrDetail", "");
+
+        //when
+        dispatcherServlet.service(request, response);
+
+        //then
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals("", response.getContentAsString()); // 기대하는 응답 내용 검증
+
+        ClientVO joinedClient = clientService.listClient(newClient.getCid());
+        assertEquals(newClient.getCid(), joinedClient.getCid());
+        assertEquals(newClient.getCpassword(), joinedClient.getCpassword());
+        assertEquals(newClient.getCname(), joinedClient.getCname());
+        assertEquals(newClient.getCnick(), joinedClient.getCnick());
+        assertEquals(newClient.getCphone(), joinedClient.getCphone());
+        assertEquals(newClient.getCaddress(), joinedClient.getCaddress());
+        assertEquals(0, joinedClient.getCreport());
+        assertEquals("N", joinedClient.getCdelete());
+        assertEquals("?", joinedClient.getCgrade());
+        assertEquals("0", joinedClient.getEnable());
+
+        // 현재는 auth를 가져오는 service가 없어 임시로 mapper 사용
+        MemberVO testId = clientMapper.read("testId");
+        assertEquals(1, testId.getClientAuthList().size());
+        assertEquals("ROLE_CLIENT", testId.getClientAuthList().get(0).getAuthority());
+
+    }
+
+}
--- src/test/java/com/alt/service/ClientServiceImplTest.java
+++ src/test/java/com/alt/service/ClientServiceImplTest.java
@@ -2,9 +2,13 @@ package com.alt.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.alt.domain.ClientAuthVO;
 import com.alt.domain.ClientVO;
+import com.alt.domain.MemberVO;
+import com.alt.mapper.ClientMapper;
 import lombok.Setter;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +28,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class ClientServiceImplTest {
 
     @Setter(onMethod_ = @Autowired)
+    private ClientMapper clientMapper;
+
+    @Setter(onMethod_ = @Autowired)
     private ClientServiceImpl clientService;
 
     @Test
-    public void testRegister() {
+    @DisplayName("신규 사용자가 회원가입을 하면 회원가입이 완료된다")
+    public void whenJoinNewClientThenSuccess() {
 
         //given
-        ClientVO newClient;
-        newClient = new ClientVO();
+        ClientVO newClient = new ClientVO();
         newClient.setCid("testId");
         newClient.setCpassword("testPassword");
         newClient.setCname("testName");
@@ -39,10 +46,32 @@ public class ClientServiceImplTest {
         newClient.setCphone("010-1234-1234");
         newClient.setCaddress("서울시 강남구");
 
+        ClientAuthVO newClientAuth = new ClientAuthVO();
+        newClientAuth.setCid("testId");
+        newClientAuth.setAuthority("ROLE_CLIENT");
+
         //when
-        String register = clientService.register(newClient);
+        String registeredResult = clientService.register(newClient);
+        String roleRegisteredResult = clientService.Roleregister(newClientAuth);
 
+        //then
+        ClientVO joinedClient = clientService.listClient("testId");
+        assertEquals("register", registeredResult);
+        assertEquals(newClient.getCid(), joinedClient.getCid());
+        assertEquals(newClient.getCpassword(), joinedClient.getCpassword());
+        assertEquals(newClient.getCname(), joinedClient.getCname());
+        assertEquals(newClient.getCnick(), joinedClient.getCnick());
+        assertEquals(newClient.getCphone(), joinedClient.getCphone());
+        assertEquals(newClient.getCaddress(), joinedClient.getCaddress());
+        assertEquals(0, joinedClient.getCreport());
+        assertEquals("N", joinedClient.getCdelete());
+        assertEquals("?", joinedClient.getCgrade());
+        assertEquals("0", joinedClient.getEnable());
 
+        // 현재는 auth를 가져오는 service가 없어 임시로 mapper 사용
+        MemberVO testId = clientMapper.read("testId");
+        assertEquals(1, testId.getClientAuthList().size());
+        assertEquals("ROLE_CLIENT", testId.getClientAuthList().get(0).getAuthority());
 
     }
 }
